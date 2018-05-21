fd5d30424aa7be6bf523abcb7934e625077dfd9a
Homer Walden <homer.walden@gmail.com>
2018-05-21 18:58:34 -0400

    Lab 07 - Input

---
+++ lab_input/input_ip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Written By: Homer Walden
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') # user_input stores the IPv4 ip address
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
