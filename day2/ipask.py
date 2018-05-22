0affd1a03b0d164df7dba0523894b24c4ff0f1f9
Homer Walden <homer.walden@gmail.com>
2018-05-22 12:01:07 -0400

    Created a script IP and MAC input variable exercise 05/22/2018

---
+++ day2/ipask.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# This script is used to collect a user's IP and MAC address and store each into a variable.
+# Written by Homer Walden
+
+#prompt user for IP address
+print('What is your IP address?') 
+hwip = input() #user IP address input
+
+#prompt user for MAC address
+print('What is your MAC address?') 
+hwmac= input() #user MAC address input
+
+#print fn of user's IP and MAC address
+print('You said your IP address is ' + str(hwip) + ' and your MAC address is ' + str(hwmac)) 
