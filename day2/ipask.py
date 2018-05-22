42a3f407864064c81d619f6a051fe25fd83a2a2c
Homer Walden <homer.walden@gmail.com>
2018-05-22 12:10:54 -0400

    Updated script IP and MAC input variable exercise 05/22/2018

--- day2/ipask.py
+++ day2/ipask.py
@@ -6,9 +6,11 @@
 print('What is your IP address?') 
 hwip = input() #user IP address input
 
+
 #prompt user for MAC address
 print('What is your MAC address?') 
 hwmac= input() #user MAC address input
+
 
 #print fn of user's IP and MAC address
 print('You said your IP address is ' + str(hwip) + ' and your MAC address is ' + str(hwmac)) 
