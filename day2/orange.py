6c82ed5b399ac312548444a5fef7dcd35524a87d
Homer Walden <homer.walden@gmail.com>
2018-05-22 11:27:44 -0400

    Created py for illustrating print functionality 05/22/2018

---
+++ day2/orange.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# The code below illustrates the use of the print function to display the word ORANGE in the middle of the screen.
+# Written by Homer Walden
+
+print(end=' '), print(end=' '), print(end=' '), print(end=' '), print(end=' '), print("ORANGE") # using 6 print fns to display the word ORANGE
